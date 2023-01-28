d4eb4c4437019305bc3eb5a91f6419189060c729
Alexander Jaffray <alexander.jaffray@gmail.com>
2023-01-27 18:25:57 -0800

    initial parallelization attempt

--- src/sensitivity_analysis.jl
+++ src/sensitivity_analysis.jl
@@ -1,32 +1,145 @@
 using BenchmarkTools
 using Tullio
+using Plots
+using FFTW
+using Zygote
+using CUDA
 
-## Version of Matrix-Vector Multiplication using Tullio.jl. Supposedly very fast and flexible.
-function EHmul(x, nodes::AbstractArray, positions::AbstractArray)
+plotlyjs()
 
-    @tullio EH[n, k] := exp <| (1.0im * pi * 2.0 * $positions[i, n] * nodes[i, k])
-    @tullio y[n] := EH[n, k] * $x[k]
+## Naive version of explicit Forward operation
+function Emul(x, shape, nodes::AbstractArray)
+
+    K = size(nodes,2)
+    x = reshape(x,shape)
+    out = zeros(ComplexF64,K)
+    X,Y = shape
+    
+    for nx = 1:X
+        for ny = 1:Y
+            for k  = 1:K
+                out[k] += x[nx,ny] * exp(-2*1im*pi*(nodes[1,k]*(nx-X/2-1)+nodes[2,k]*(ny-Y/2-1)))
+            end
+        end
+    end
+
+    return vec(out)
+
+end
+
+function kernel(a)
+
+
+
+end
+
+function computePosForward(shape)
+
+    mapreduce(permutedims,vcat,hcat(collect.(Tuple.(CartesianIndices(shape))))[:])' .- [shape[1]÷2, shape[2]÷2] .- [1, 1]
+
+end
+
+function computeNodesBackward(nodes,positions)
+
+
+
+end
+
+function par_Emul(x,shape,nodes::AbstractArray, positions::AbstractArray)
+
+    K = size(nodes,2)
+    x = reshape(x,shape)
+    y = zeros(ComplexF64,K)
+    X,Y = shape
+
+    stride = 256
+    extra = mod(prod(shape),stride)  
+    nblocks = prod(shape) ÷ stride      
+    for k = 1:K
+        for nx = 1:nblocks
+            @info size(exp.(-2*1im*pi*(nodes[:,k]'*positions[:,(nx-1)*stride .+ (1:stride)]))*x[(nx-1)*stride .+ (1:stride)])
+        end
+    end
 
     return y
 
 end
 
-## Version of Matrix-Vector Multiplication using Tullio.jl. Supposedly very fast and flexible.
-function EMul(x, nodes::AbstractArray, positions::AbstractArray)
+## Naive version of explicit adjoint op
+function EHmul(x, shape, nodes::AbstractArray)
 
-    @tullio E[k, n] := exp <| (-1.0im * pi * 2.0 * nodes[i, k] * $positions[i, n])
-    @tullio y[k] := E[k, n] * $x[n]
+    K = size(nodes,2)
+    out = zeros(ComplexF64,shape)
+    X,Y = shape
+    
+    for nx = 1:X
+        for ny = 1:Y
+            for k  = 1:K
+                out[nx,ny] += x[k] * conj(exp(-2*1im*pi*(nodes[1,k]*(nx-X/2-1)+nodes[2,k]*(ny-Y/2-1))))
+            end
+        end
+    end
 
-    return y
+    return 1/prod(shape)*vec(out)
 
 end
 
-positions = LinRange(-64,63,128)
-positions = vcat(positions',ones(size(positions))')
-b = randn(ComplexF64,size(positions,2))
-nodes = LinRange(-0.5,0.5,128)
-nodes = vcat(nodes',ones(size(nodes))')
+## Generate Cartesian Nodes
+function cartesian2dNodes(::Type{T}, numProfiles, numSamplingPerProfile; kmin=(-0.5,-0.5), kmax=(0.5,0.5), kargs...) where T
 
-y = 1.0/sqrt(128) .* EMul(b,nodes,positions)
-y2 = 1.0/sqrt(128) .* EHmul(y,nodes,positions)
+    nodes = zeros(T, 2, numSamplingPerProfile, numProfiles)
+    posX = collect( -ceil(Int64, (numSamplingPerProfile-1)/2.):floor(Int64, (numSamplingPerProfile-1)/2.) ) / numSamplingPerProfile
+    posY = collect( -ceil(Int64, (numProfiles-1)/2.):floor(Int64, (numProfiles-1)/2.) ) / numProfiles
 
+    for l = 1:numProfiles
+        for k = 1:numSamplingPerProfile
+            nodes[1,k,l] = posX[k]
+            nodes[2,k,l] = posY[l]
+        end
+    end
+    
+    # rescale nodes according to the region covered by kmin & kmax
+    nodes[1,:,:] .*= (kmax[1]-kmin[1])
+    nodes[2,:,:] .*= (kmax[2]-kmin[2])
+    
+    # shift nodes to the region defined by kmin & kmax
+    nodes[1,:,:] .+= 0.5*(kmin[1]+kmax[1])
+    nodes[2,:,:] .+= 0.5*(kmin[2]+kmax[2])
+
+    return reshape(nodes, 2, numSamplingPerProfile*numProfiles)
+
+end
+
+function jacobi(f, x)
+    y, back = Zygote.pullback(f, x)
+    back(1)[1], back(im)[1]
+end
+  
+function wirtinger(f, x)
+    du, dv = jacobi(f, x)
+    (du' + im*dv')/2, (du + im*dv)/2
+end
+
+mag_test = x->(sinc(0.2*x))
+phase_test = x->(0.01*x)
+positions = LinRange(-64,63,64)
+
+mag = mag_test.(positions) .* mag_test.(positions)'
+phase = phase_test.(positions) .* phase_test.(positions)'
+
+p1 = plot(mag)
+p2 = plot(phase)
+
+x = mag .* exp.(1im .* 2 .*pi .*phase)
+
+x_cu = CuArray(x)
+
+nodes = cartesian2dNodes(Float64,32,64)
+
+nodes_cu = CuArray(nodes)
+shape = size(x)
+
+ft_x = Emul(x,shape,nodes)
+x̂ = EHmul(ft_x,shape,nodes)
+
+@benchmark EHmul(ft_x,shape,nodes)
